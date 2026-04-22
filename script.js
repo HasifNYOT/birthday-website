0a4b7d274cfaa5c52668938cf09d58a99aaf50cb
hasif.kamaruzaman <hasif.kamaruzaman@sapura.com.my>
2026-04-22 14:16:02 +0800

    change petal spawn position

--- script.js
+++ script.js
@@ -80,12 +80,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    // --- Rose Petal Animation ---
+    // --- Sakura Petal Animation ---
     const canvas = document.getElementById('sakura-canvas');
     if (canvas) {
         const ctx = canvas.getContext('2d');
         let petals = [];
-        const numPetals = 35;
+        // const numPetals = 50;
+        const numPetals = 15;
 
         function resizeCanvas() {
             canvas.width = window.innerWidth;
@@ -97,53 +98,15 @@ document.addEventListener('DOMContentLoaded', function() {
         function Petal() {
             this.x = Math.random() * canvas.width;
             this.y = Math.random() * canvas.height * 2 - canvas.height;
-            this.w = 35 + Math.random() * 20;
-            this.h = 30 + Math.random() * 15;
-            this.opacity = 0.7 + Math.random() * 0.3;
-            this.xSpeed = 0.8 + Math.random() * 1.2;
-            this.ySpeed = 0.8 + Math.random() * 1.2;
-            this.rotation = Math.random() * Math.PI * 2;
-            this.rotationSpeed = (Math.random() - 0.5) * 0.03;
+            this.w = 35 + Math.random() * 25;
+            this.h = 30 + Math.random() * 20;
+            this.opacity = this.w / 40;
+            this.flip = Math.random();
+            this.xSpeed = 1.5 + Math.random() * 2;
+            this.ySpeed = 1 + Math.random() * 1;
+            this.flipSpeed = Math.random() * 0.03;
         }
-        // function Petal() {
-    //         this.x = Math.random() * canvas.width;
-    //         this.y = Math.random() * canvas.height * 2 - canvas.height;
-    //         this.w = 25 + Math.random() * 15;
-    //         this.h = 20 + Math.random() * 10;
-    //         this.opacity = this.w / 40;
-    //         this.flip = Math.random();
-    //         this.xSpeed = 1.5 + Math.random() * 2;
-    //         this.ySpeed = 1 + Math.random() * 1;
-    //         this.flipSpeed = Math.random() * 0.03;
-    //     }
 
-        // Petal.prototype.draw = function() {
-        //     ctx.save();
-        //     ctx.globalAlpha = this.opacity;
-        //     ctx.translate(this.x, this.y);
-        //     ctx.rotate(this.rotation);
-
-        //     ctx.beginPath();
-        //     ctx.moveTo(0, -this.h / 2);
-        //     ctx.bezierCurveTo(
-        //         this.w / 2, -this.h / 2,
-        //         this.w / 2,  this.h / 2,
-        //         0,           this.h / 2
-        //     );
-        //     ctx.bezierCurveTo(
-        //         -this.w / 2, this.h / 2,
-        //         -this.w / 2, -this.h / 2,
-        //         0,           -this.h / 2
-        //     );
-        //     ctx.closePath();
-
-        //     const gradient = ctx.createRadialGradient(0, 0, 1, 0, 0, this.w / 2);
-        //     gradient.addColorStop(0, '#FF6B8A');
-        //     gradient.addColorStop(1, '#C0003C');
-        //     ctx.fillStyle = gradient;
-        //     ctx.fill();
-        //     ctx.restore();
-        // }
         Petal.prototype.draw = function() {
             if (this.y > canvas.height || this.x > canvas.width) {
                 this.x = -this.w;
@@ -160,21 +123,12 @@ document.addEventListener('DOMContentLoaded', function() {
             ctx.closePath();
             ctx.fillStyle = '#C0003C';
             ctx.fill();
-            ctx.restore();
         }
 
         Petal.prototype.update = function() {
             this.x += this.xSpeed;
             this.y += this.ySpeed;
-            this.rotation += this.rotationSpeed;
-
-            // Reset petal when it goes off screen
-            if (this.y > canvas.height || this.x > canvas.width) {
-                this.x = 0;
-                this.y = 0;
-                this.xSpeed = 0.8 + Math.random() * 1.2;
-                this.ySpeed = 0.8 + Math.random() * 1.2;
-            }
+            this.flip += this.flipSpeed;
             this.draw();
         }
 
@@ -187,84 +141,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
         function animate() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            petals.forEach(petal => petal.update());
+            petals.forEach(petal => {
+                petal.update();
+            });
             requestAnimationFrame(animate);
         }
 
         createPetals();
         animate();
     }
-
-
-    // // --- Sakura Petal Animation ---
-    // const canvas = document.getElementById('sakura-canvas');
-    // if (canvas) {
-    //     const ctx = canvas.getContext('2d');
-    //     let petals = [];
-    //     // const numPetals = 50;
-    //     const numPetals = 35;
-
-    //     function resizeCanvas() {
-    //         canvas.width = window.innerWidth;
-    //         canvas.height = window.innerHeight;
-    //     }
-    //     window.addEventListener('resize', resizeCanvas);
-    //     resizeCanvas();
-
-    //     function Petal() {
-    //         this.x = Math.random() * canvas.width;
-    //         this.y = Math.random() * canvas.height * 2 - canvas.height;
-    //         this.w = 25 + Math.random() * 15;
-    //         this.h = 20 + Math.random() * 10;
-    //         this.opacity = this.w / 40;
-    //         this.flip = Math.random();
-    //         this.xSpeed = 1.5 + Math.random() * 2;
-    //         this.ySpeed = 1 + Math.random() * 1;
-    //         this.flipSpeed = Math.random() * 0.03;
-    //     }
-
-    //     Petal.prototype.draw = function() {
-    //         if (this.y > canvas.height || this.x > canvas.width) {
-    //             this.x = -this.w;
-    //             this.y = Math.random() * canvas.height * 2 - canvas.height;
-    //             this.xSpeed = 1.5 + Math.random() * 2;
-    //             this.ySpeed = 1 + Math.random() * 1;
-    //             this.flip = Math.random();
-    //         }
-    //         ctx.globalAlpha = this.opacity;
-    //         ctx.beginPath();
-    //         ctx.moveTo(this.x, this.y);
-    //         ctx.bezierCurveTo(this.x + this.w / 2, this.y - this.h / 2, this.x + this.w, this.y, this.x + this.w / 2, this.y + this.h / 2);
-    //         ctx.bezierCurveTo(this.x, this.y + this.h, this.x - this.w / 2, this.y, this.x, this.y);
-    //         ctx.closePath();
-    //         ctx.fillStyle = '#C0003C';
-    //         ctx.fill();
-    //     }
-
-    //     Petal.prototype.update = function() {
-    //         this.x += this.xSpeed;
-    //         this.y += this.ySpeed;
-    //         this.flip += this.flipSpeed;
-    //         this.draw();
-    //     }
-
-    //     function createPetals() {
-    //         petals = [];
-    //         for (let i = 0; i < numPetals; i++) {
-    //             petals.push(new Petal());
-    //         }
-    //     }
-
-    //     function animate() {
-    //         ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //         petals.forEach(petal => {
-    //             petal.update();
-    //         });
-    //         requestAnimationFrame(animate);
-    //     }
-
-    //     createPetals();
-    //     animate();
-    // }
 });
 
